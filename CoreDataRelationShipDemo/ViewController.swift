3c76a62a9d99eaccb1334f9ae60c6e4aa739cae4
Raghavendra <raghudattawad>
2020-01-14 07:15:52 +0530

    Initial Commit

---
+++ CoreDataRelationShipDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataRelationShipDemo
+//
+//  Created by Raghavendra on 14/01/20.
+//  Copyright © 2020 To Be Success echnology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
